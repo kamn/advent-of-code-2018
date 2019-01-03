f768377e1865dfa88e25b5926ade21015dd1df19
unknown <kamn.miller@gmail.com>
2019-01-02 21:11:59 -0700

    Initial, quick completion of 1.1

---
+++ AdventOfCode2018/Challenge1.fs
@@ -0,0 +1,19 @@
+﻿module Challenge1
+
+open System
+open System.IO
+
+let readFile fileName =
+    let text = File.ReadAllText(Environment.CurrentDirectory + "\\" + fileName)
+    let splitLines = text.Split([|"\r\n"; "\n"; "\r"|], StringSplitOptions.RemoveEmptyEntries)
+    splitLines
+
+let listToInt xs = 
+    List.map int xs
+
+
+let solve () =
+    let strData = readFile "c1.txt"
+    let intData = listToInt (strData |> Array.toList)
+    let reduced = intData |> List.reduce (fun r x -> r + x)
+    printf "%A" reduced
--- AdventOfCode2018/Program.fs
+++ AdventOfCode2018/Program.fs
@@ -1,7 +1,12 @@
 ﻿// Learn more about F# at http://fsharp.org
 // See the 'F# Tutorial' project for more help.
 
+open System
+module c1 =  Challenge1
 [<EntryPoint>]
 let main argv = 
     printfn "%A" argv
+    c1.solve ()
+    Console.ReadLine()
+
     0 // return an integer exit code
